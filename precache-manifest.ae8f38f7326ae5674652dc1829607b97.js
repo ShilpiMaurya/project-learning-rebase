6a95ae30b76be3c595d8d54edf4f88cb0941952c
ShilpiMaurya <meshilpi94@gmail.com>
2020-01-19 22:46:42 +0530

    Updates

---
+++ precache-manifest.ae8f38f7326ae5674652dc1829607b97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c05204064efbdb6e1ce2c4395ef3dd",
+    "url": "/project-learning-rebase/index.html"
+  },
+  {
+    "revision": "dbc52c9b4a226a9bf42c",
+    "url": "/project-learning-rebase/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "da010b158d7a982490b4",
+    "url": "/project-learning-rebase/static/js/2.45c08c70.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/project-learning-rebase/static/js/2.45c08c70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dbc52c9b4a226a9bf42c",
+    "url": "/project-learning-rebase/static/js/main.4ce121ab.chunk.js"
+  },
+  {
+    "revision": "d66a910ced5fd9adf182",
+    "url": "/project-learning-rebase/static/js/runtime-main.e859ee14.js"
+  }
+]);
