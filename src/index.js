01376da4c11adfabd57bf9998a02756fd079fe0c
ShilpiMaurya <meshilpi94@gmail.com>
2020-01-19 13:47:28 +0530

    third commit of master

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Primary from "./Primary";
+import Secondary from "./Secondary";
 import "./index.css";
 const App = () => {
   return (
     <>
       <h1>Let's Learn Rebasing</h1>
       <h4>Become the best version of yourself everyday</h4>
+      <Primary />
+      <Secondary />
     </>
   );
 };
